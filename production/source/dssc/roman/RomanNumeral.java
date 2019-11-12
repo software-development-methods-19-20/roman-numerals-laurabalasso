6dcdcfe0237e5ef292e3d78efc6e28e714613f67
laurabalasso <laurabalasso96@gmail.com>
2019-11-12 09:45:05 +0100

    Convert 1 to I

---
+++ production/source/dssc/roman/RomanNumeral.java
@@ -0,0 +1,26 @@
+package dssc.roman;
+
+public class RomanNumeral {
+
+
+    private int decimal;
+
+    public RomanNumeral(int decimal) {
+
+        this.decimal = decimal;
+    }
+
+    @Override
+    public String toString() {
+
+        if(this.decimal==0){
+            return "";
+        }
+        else{
+            return "I";
+        }
+
+    }
+
+
+}
--- test/source/test/roman/DecimalToRomanNumeralTest.java
+++ test/source/test/roman/DecimalToRomanNumeralTest.java
@@ -1,5 +1,22 @@
 package test.roman;
 
+import dssc.roman.RomanNumeral;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DecimalToRomanNumeralTest {
 
+    @Test
+    void zeroIsEmptyString() {
+        RomanNumeral romanNumeral = new RomanNumeral(0);
+        assertEquals("", romanNumeral.toString());
+    }
+
+    @Test
+    void oneIsI() {
+        RomanNumeral romanNumeral = new RomanNumeral(1);
+        assertEquals("I", romanNumeral.toString());
+    }
 }
